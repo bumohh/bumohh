fe1d59c0cd87d1162a468746423f736b939530fe
ILuvHoneyBun <phantommy24@yahoo.com>
2021-06-02 13:50:48 -0500

    fixing cart view controller

---
+++ Project3/CartViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CartViewController.swift
+//  Project3
+//
+//  Created by Tommy Phan on 6/2/21.
+//
+
+import UIKit
+
+class CartViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+}
